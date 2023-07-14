f2f2b03165ba71620ea08f35082e4c1e2933df05
dpinchuk <dimapinchuk2001@gmail.com>
2023-07-14 04:51:04 +0000

    Neccessary for pulling main

--- dmvfn/CVPR2023-DMVFN/scripts/train.sh
+++ dmvfn/CVPR2023-DMVFN/scripts/train.sh
@@ -1,11 +1,11 @@
-torchrun --nproc_per_node=2 \
+torchrun --nproc_per_node=1 \
 --master_port=4321 ./train.py \
 --epoch 2 \
 --train_dataset hic \
 --val_datasets hic \
 --batch_size 8 \
 --resume_path ./../pretrained_models/dmvfn_city.pkl \
---num_gpu 2 \
+--num_gpu 1 \
 --num_workers 1 \
 --resume_epoch 1 \
---code_test
+--no_code_test
