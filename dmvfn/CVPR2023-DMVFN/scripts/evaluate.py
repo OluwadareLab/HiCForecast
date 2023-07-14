1eb3e905bfd2f5e1edd45ad0864313b1a1225b64
dpinchuk <dimapinchuk2001@gmail.com>
2023-07-14 20:45:08 +0000

    Changed genomic distance for pearson correlation and added pearson correlation evaluation

--- dmvfn/CVPR2023-DMVFN/scripts/evaluate.py
+++ dmvfn/CVPR2023-DMVFN/scripts/evaluate.py
@@ -8,17 +8,19 @@ from GenomeDISCO import*
 from ssim import*
 
 
-pred = np.load("./../data/chr6_predicted_final_untrained_224.npy")
-ground_truth = np.load("./../data/ground_truth_chr6.npy")
+pred = np.load("/home/ubuntu/dpinchuk/dmvfn/CVPR2023-DMVFN/data/data_64/predictions/single_channel_no_vgg_64/epoch_20/data_pred_chr6_final.npy")
+ground_truth = np.load("/home/ubuntu/dpinchuk/dmvfn/CVPR2023-DMVFN/data/data_64/data_gt_chr6_64.npy")
 print("pred.shape: ", pred.shape)
 print("ground_truth.shape: ", ground_truth.shape)
+pearson_list = []
+for i in range(3):
+    r = diagcorr(pred[i], ground_truth[3+i])
+    pearson_list.append(r)
 
-r1 = diagcorr(pred[0], ground_truth[3])
-r2 = diagcorr(pred[1], ground_truth[4])
-r3 = diagcorr(pred[2], ground_truth[5])
 
-disco1 = compute_reproducibility(pred[0], ground_truth[3], True)
-print("disco1: ", disco1)
+print("pearson_list: ", pearson_list)
+#disco1 = compute_reproducibility(pred[0], ground_truth[3], True)
+#print("disco1: ", disco1)
 
 #ssim1 = ssim(torch.from_numpy(pred[0]).unsqueeze(0).unsqueeze(0), torch.from_numpy(ground_truth[3]).unsqueeze(0).unsqueeze(0), window_size=5)
 #print("ssim1: ", ssim1)
