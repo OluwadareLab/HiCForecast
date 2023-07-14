671e42cc8bfb9275a32d6717c1ab362074fd5a5f
dpinchuk <dimapinchuk2001@gmail.com>
2023-07-14 05:13:41 +0000

    Fixed train.sh

--- dmvfn/CVPR2023-DMVFN/scripts/train.sh
+++ dmvfn/CVPR2023-DMVFN/scripts/train.sh
@@ -1,5 +1,5 @@
 torchrun --nproc_per_node=2 \
---master_port=4321 ./train_1d.py \
+--master_port=4321 ./train.py \
 --epoch 25 \
 --train_dataset hic \
 --val_datasets hic \
@@ -7,5 +7,6 @@ torchrun --nproc_per_node=2 \
 --num_gpu 2 \
 --num_workers 1 \
 --resume_path /home/dmitryp/dpinchuk/dmvfn/CVPR2023-DMVFN/models/hic_train_log/20230712-225137/dmvfn_14.pkl \
---resume_epoch 14
+--resume_epoch 14 \
 --no_code_test
+
