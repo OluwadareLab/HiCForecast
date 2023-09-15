76c8594da550dc9cdad647ceda39e728664ebd33
dpinchuk <dimapinchuk2001@gmail.com>
2023-09-15 01:54:14 +0000

    Fixed bugs with optmizer and added new models

--- dmvfn/model/model_1d.py
+++ dmvfn/model/model_1d.py
@@ -65,16 +65,14 @@ class Model:
 
             loss_l1, loss_vgg = 0, 0
             loss_mse = 0
-            l1_loss = 0
             for i in range(9):
                 if self.loss == 'single_channel_no_vgg' or self.loss == 'single_channel_default_VGG':
                     loss_l1 +=  (self.lap(merged[i], gt)).mean()*(0.8**(8-i))
-                if self.loss == 'single_channel_MSE_no_vgg':
+                if self.loss == 'single_channel_MSE_no_vgg' or self.loss == 'single_channel_MSE_VGG':
                     loss_mse += (self.MSELoss(merged[i], gt)).mean()*(0.8**(8-i)) 
-                if self.loss == 'single_channel_L1_no_vgg':
+                if self.loss == 'single_channel_L1_no_vgg' or 'single_channel_L1_VGG':
                     loss_l1 += (self.l1_loss(merged[i], gt)).mean()*(0.8**(8-i)) 
-
-            if self.loss == 'single_channel_default_VGG':
+            if self.loss == 'single_channel_default_VGG' or self.loss == 'single_channel_MSE_VGG' or self.loss == 'single_channel_L1_VGG':
                 merged_sq = torch.squeeze(merged[-1])
                 merged_rgb = torch.stack((merged_sq, merged_sq, merged_sq))
                 merged_rgb = torch.permute(merged_rgb, (1,0,2,3))
@@ -83,7 +81,7 @@ class Model:
                 gt_rgb = torch.permute(gt_rgb, (1,0,2,3))
                 loss_vgg = (self.vggloss(merged_rgb, gt_rgb)).mean()
                 #loss_vgg = (self.vggloss(merged[-1], gt)).mean()
-                loss_G = loss_mse + loss_vgg * 0.5
+                loss_G = loss_mse + loss_l1 + loss_vgg * 0.5
 
             self.optimG.zero_grad()
             if self.loss == 'single_channel_no_vgg' or self.loss == 'single_channel_L1_no_vgg':
--- dmvfn/scripts/train_1d.py
+++ dmvfn/scripts/train_1d.py
@@ -138,6 +138,9 @@ logger = logging.getLogger('base')
 for arg, value in sorted(vars(args).items()):
     logger.info("{} Argument {}: {}".format(get_formatted_timestamp(), arg, value))
 
+if lr == None:
+    logger.info("Defualt learning rate schedule")
+
 def train(model, args):
     step = 0
     nr_eval = args.resume_epoch
@@ -262,99 +265,13 @@ def evaluate(model, data_val_path, name, epoch, step):
         print("Calling assemble.py")
         get_predictions(val_save_path, 1534, patch_size) #assemble into one big matrix
         val_hicrep = get_hicrep(val_save_path + "pred_chr19_final.npy", patch_size, 400000)
+        val_hicrep_40k = get_hicrep(val_save_path + "pred_chr19_final.npy", patch_size, 40000)
         logger.info("Validation scores: {}".format(val_hicrep))
         for i in range(3):
             writer_val.add_scalar(name+' hicrep_%d'%(i),  val_hicrep[i], epoch)
+            writer_val.add_scalar(name+' hicrep_40k_%d'%(i),  val_hicrep_40k[i], epoch)
         return val_hicrep
 
-'''
-    if name == "CityValDataset" or name == "KittiValDataset" or name == "DavisValDataset":
-        with torch.no_grad():
-            lpips_score_mine, psnr_score_mine, msssim_score_mine, ssim_score_mine = np.zeros(5), np.zeros(5), np.zeros(5), np.zeros(5)
-            time_stamp = time.time()
-            num = val_data.__len__()
-            for i, data in enumerate(val_data):
-                data_gpu, _ = data
-                data_gpu = data_gpu.to(device, non_blocking=True) / 255.
-                preds = model.eval(data_gpu, name)
-
-                b,n,c,h,w = preds.shape
-                assert b==1 and n==5
-
-                gt, pred = data_gpu[0], preds[0]
-                for j in range(5):
-                    psnr = -10 * math.log10(torch.mean((gt[j+4] - pred[j]) * (gt[j+4] - pred[j])).cpu().data)
-                    ssim_val = ssim( gt[j+4:j+5], pred[j:j+1], data_range=1.0, size_average=False) # return (N,)
-                    ms_ssim_val = ms_ssim( gt[j+4:j+5], pred[j:j+1], data_range=1.0, size_average=False ) #(N,)
-                    x, y = ((gt[j+4:j+5]-0.5)*2.0).clone(), ((pred[j:j+1]-0.5)*2.0).clone()
-                    lpips_val = loss_fn_alex(x, y)
-
-                    lpips_score_mine[j] += lpips_val
-                    ssim_score_mine[j] += ssim_val
-                    msssim_score_mine[j] += ms_ssim_val
-                    psnr_score_mine[j] += psnr
-                    
-                    gt_1 = (gt[j+4:j+5].permute(0, 2, 3, 1).cpu().numpy() * 255).astype('uint8')
-                    pred_1 = (pred[j:j+1].permute(0, 2, 3, 1).cpu().numpy() * 255).astype('uint8')
-                    if i == 50 and local_rank == 0:
-                            imgs = np.concatenate((gt_1[0], pred_1[0]), 1)[:, :, ::-1]
-                            writer_val.add_image(name+str(j) + '/img', imgs.copy(), step, dataformats='HWC')
-            eval_time_interval = time.time() - time_stamp
-            if local_rank != 0:
-                return
-            psnr_score_mine, ssim_score_mine, msssim_score_mine, lpips_score_mine = psnr_score_mine/num, ssim_score_mine/num, msssim_score_mine/num, lpips_score_mine/num
-            for i in range(5):
-                logger.info('%d             '%(nr_eval)+name+'  psnr_%d     '%(i)+'%.4f'%(sum(psnr_score_mine[:(i+1)])/(i+1))+'  ssim_%d     '%(i)+'%.4f'%(sum(ssim_score_mine[:(i+1)])/(i+1))+'  ms_ssim_%d     '%(i)+
-                '%.4f'%(sum(msssim_score_mine[:(i+1)])/(i+1))+'  lpips_%d     '%(i)+'%.4f'%(sum(lpips_score_mine[:(i+1)])/(i+1)))
-
-                writer_val.add_scalar(name+' psnr_%d'%(i),  psnr_score_mine[i], step)
-                writer_val.add_scalar(name+' ssim_%d'%(i),  ssim_score_mine[i], step)
-                writer_val.add_scalar(name+' ms_ssim_%d'%(i),  msssim_score_mine[i], step)
-                writer_val.add_scalar(name+' lpips_%d'%(i),  lpips_score_mine[i], step)
-    elif name=="VimeoValDataset":
-        with torch.no_grad():
-            lpips_score_mine, ssim_score_mine, msssim_score_mine, psnr_score_mine   = np.zeros(1), np.zeros(1), np.zeros(1), np.zeros(1)
-            time_stamp = time.time()
-            num = val_data.__len__()
-            for i, data in enumerate(val_data):
-                data_gpu, _ = data
-                data_gpu = data_gpu.to(device, non_blocking=True) / 255.
-                preds = model.eval(data_gpu, name)
-
-                b,n,c,h,w = preds.shape
-                assert b==1 and n==1
-
-                gt, pred = data_gpu[0], preds[0]
-                psnr = -10 * math.log10(torch.mean((gt[2] - pred[0]) * (gt[2] - pred[0])).cpu().data)
-                ssim_val = ssim( gt[2:3], pred[0:1], data_range=1.0, size_average=False) # return (N,)
-                ms_ssim_val = ms_ssim( gt[2:3], pred[0:1], data_range=1.0, size_average=False ) #(N,)
-                x, y = ((gt[2:3]-0.5)*2.0).clone(), ((pred[0:1]-0.5)*2.0).clone()
-                lpips_val = loss_fn_alex(x, y)
-
-                lpips_score_mine[0] += lpips_val
-                ssim_score_mine[0] += ssim_val
-                msssim_score_mine[0] += ms_ssim_val
-                psnr_score_mine[0] += psnr
-                
-                gt_1 = (gt[2:3].permute(0, 2, 3, 1).cpu().numpy() * 255).astype('uint8')
-                pred_1 = (pred[0:1].permute(0, 2, 3, 1).cpu().numpy() * 255).astype('uint8')
-                if i == 50 and local_rank == 0:
-                        imgs = np.concatenate((gt_1[0], pred_1[0]), 1)[:, :, ::-1]
-                        writer_val.add_image(name+str(0) + '/img', imgs.copy(), step, dataformats='HWC')
-            eval_time_interval = time.time() - time_stamp
-
-            if local_rank != 0:
-                return
-            psnr_score_mine, ssim_score_mine, msssim_score_mine, lpips_score_mine = psnr_score_mine/num, ssim_score_mine/num, msssim_score_mine/num, lpips_score_mine/num
-            for i in range(1):
-                logger.info('%d             '%(nr_eval)+name+'  psnr_%d     '%(i)+'%.4f'%(sum(psnr_score_mine[:(i+1)])/(i+1))+'  ssim_%d     '%(i)+'%.4f'%(sum(ssim_score_mine[:(i+1)])/(i+1))+'  ms_ssim_%d     '%(i)+
-                '%.4f'%(sum(msssim_score_mine[:(i+1)])/(i+1))+'  lpips_%d     '%(i)+'%.4f'%(sum(lpips_score_mine[:(i+1)])/(i+1)))
-
-                writer_val.add_scalar(name+' psnr_%d'%(i),  psnr_score_mine[i], step)
-                writer_val.add_scalar(name+' ssim_%d'%(i),  ssim_score_mine[i], step)
-                writer_val.add_scalar(name+' ms_ssim_%d'%(i),  msssim_score_mine[i], step)
-                writer_val.add_scalar(name+' lpips_%d'%(i),  lpips_score_mine[i], step)
-'''
 
 def predict(model, data_path, output_dir, cut_off):
     with torch.no_grad():
@@ -370,26 +287,6 @@ def predict(model, data_path, output_dir, cut_off):
         test_loader = torch.utils.data.DataLoader(dat_test[:,1:3], batch_size=1, shuffle=False)
         print("dat_test.shape: ", dat_test.shape)
         
-        '''
-        image_0 = dat_test[600][1]
-        image_1 = dat_test[600][2]
-        img_0 = np.repeat(image_0[np.newaxis, :], 3, axis=0).astype('float32')
-        print("img_0.shape: ", img_0.shape)
-        
-        img_1 = np.repeat(image_1[np.newaxis, :], 3, axis=0).astype('float32')
-
-        print("img_0.shape: ", img_0.shape)#me
-        print("img_1.shape: ", img_1.shape)#ME
-
-        if img_0 is None or img_1 is None:
-            raise Exception("Images not found.")
-
-        img = torch.stack((torch.tensor(img_0), torch.tensor(img_1)), 0)
-        print("img.shape: ", img.shape)
-        img = img.unsqueeze(0).to(device, non_blocking=True) #BNCHW
-        img = img.to(device, non_blocking=True) / 255.
-        print("img.shape: ", img.shape)
-        '''
         predictions = []
         for i, X in enumerate(tqdm(test_loader)):
             # X = X.unsqueeze(0).to(device, non_blocking=True)
@@ -429,43 +326,3 @@ if __name__ == "__main__":
         logger.info(traceback.format_exc())
         exit()
         
-'''
-            for i, data in enumerate(val_data):
-                data_gpu, _ = data
-                data_gpu = data_gpu.to(device, non_blocking=True) / 255.
-                preds = model.eval(data_gpu, name)
-
-                b,n,c,h,w = preds.shape
-                assert b==1 and n==5
-
-                gt, pred = data_gpu[0], preds[0]
-                for j in range(5):
-                    psnr = -10 * math.log10(torch.mean((gt[j+4] - pred[j]) * (gt[j+4] - pred[j])).cpu().data)
-                    ssim_val = ssim( gt[j+4:j+5], pred[j:j+1], data_range=1.0, size_average=False) # return (N,)
-                    ms_ssim_val = ms_ssim( gt[j+4:j+5], pred[j:j+1], data_range=1.0, size_average=False ) #(N,)
-                    x, y = ((gt[j+4:j+5]-0.5)*2.0).clone(), ((pred[j:j+1]-0.5)*2.0).clone()
-                    lpips_val = loss_fn_alex(x, y)
-
-                    lpips_score_mine[j] += lpips_val
-                    ssim_score_mine[j] += ssim_val
-                    msssim_score_mine[j] += ms_ssim_val
-                    psnr_score_mine[j] += psnr
-                    
-                    gt_1 = (gt[j+4:j+5].permute(0, 2, 3, 1).cpu().numpy() * 255).astype('uint8')
-                    pred_1 = (pred[j:j+1].permute(0, 2, 3, 1).cpu().numpy() * 255).astype('uint8')
-                    if i == 50 and local_rank == 0:
-                            imgs = np.concatenate((gt_1[0], pred_1[0]), 1)[:, :, ::-1]
-                            writer_val.add_image(name+str(j) + '/img', imgs.copy(), step, dataformats='HWC')
-            eval_time_interval = time.time() - time_stamp
-            if local_rank != 0:
-                return
-            psnr_score_mine, ssim_score_mine, msssim_score_mine, lpips_score_mine = psnr_score_mine/num, ssim_score_mine/num, msssim_score_mine/num, lpips_score_mine/num
-            for i in range(5):
-                logger.info('%d             '%(nr_eval)+name+'  psnr_%d     '%(i)+'%.4f'%(sum(psnr_score_mine[:(i+1)])/(i+1))+'  ssim_%d     '%(i)+'%.4f'%(sum(ssim_score_mine[:(i+1)])/(i+1))+'  ms_ssim_%d     '%(i)+
-                '%.4f'%(sum(msssim_score_mine[:(i+1)])/(i+1))+'  lpips_%d     '%(i)+'%.4f'%(sum(lpips_score_mine[:(i+1)])/(i+1)))
-
-                writer_val.add_scalar(name+' psnr_%d'%(i),  psnr_score_mine[i], step)
-                writer_val.add_scalar(name+' ssim_%d'%(i),  ssim_score_mine[i], step)
-                writer_val.add_scalar(name+' ms_ssim_%d'%(i),  msssim_score_mine[i], step)
-                writer_val.add_scalar(name+' lpips_%d'%(i),  lpips_score_mine[i], step)
-'''
