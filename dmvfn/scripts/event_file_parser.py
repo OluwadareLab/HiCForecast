6763d26b5e5ce441949557e9321710c0c7bbd486
dpinchuk <dimapinchuk2001@gmail.com>
2024-03-26 01:16:50 +0000

    Added epochs 99 and 100 from final model

--- dmvfn/scripts/event_file_parser.py
+++ dmvfn/scripts/event_file_parser.py
@@ -1,8 +1,10 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import pandas as pd
 
-input_file = "./../logs/hic_train_log/20240304-001515/validate/events.out.tfevents.1709529315.a23b7e2a54c9.14033.1"
-out_file = "./../results/dflogs/20240304-001515.csv"
+#input_file = "./../logs/hic_train_log/20240304-001515/validate/events.out.tfevents.1709529315.a23b7e2a54c9.14033.1"
+#out_file = "./../results/dflogs/20240304-001515.csv"
+input_file = "./../logs/hic_train_log/20240304-001809/validate/events.out.tfevents.1709529489.f214aa2340b9.27732.1"
+out_file = "./../results/dflogs/20240304-001809.csv"
 names = ["hic disco_35_0", "hic disco_35_1", "hic disco_35_2", "hic ssim_35_0", "hic ssim_35_1", "hic ssim_35_2", "hic pearson_35_0", 
         "hic pearson_35_1", "hic pearson_35_2", "hic psnr_35_0", "hic psnr_35_1", "hic psnr_35_2", "hic hicrep_34_0", "hic hicrep_34_1", 
         "hic hicrep_34_2"]
